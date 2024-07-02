ba56190ed39b396a0cb12e74ad03ddd2f040e9a8
Tharun Atithya <tharunaadhi6@gmail.com>
2024-07-02 15:35:31 +0530

    workout

--- day2.java
+++ day2.java
@@ -92,48 +92,81 @@ public class day2 {
         //         break;
         // }
 
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the shape ");
-        String val = sc.nextLine();
-        val = val.toLowerCase().strip();
-        switch (val) {
-            case "circle":
-                System.out.println("enter radius of the circle");
-                double radi = sc.nextDouble();
-                System.out.println("perimeter of circle is " + (2*3.14*radi));
-                System.out.println("area of circle " + (3.14*(radi*radi)));
-                System.out.println(" ");
-                break;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter the shape ");
+        // String val = sc.nextLine();
+        // val = val.toLowerCase().strip();
+        // switch (val) {
+        //     case "circle":
+        //         System.out.println("enter radius of the circle");
+        //         double radi = sc.nextDouble();
+        //         System.out.println("perimeter of circle is " + (2*3.14*radi));
+        //         System.out.println("area of circle " + (3.14*(radi*radi)));
+        //         System.out.println(" ");
+        //         break;
             
-            case "square":
-                System.out.println("enter area of square");
-                double area = sc.nextDouble();
-                System.out.println("perimetr od square " + (4*area));
-                System.out.println("area of square " + (area * area));
-                System.out.println(" ");
-                break;
-            case "rectangle":
-                System.out.println("enter length of rectangle");
-                double length = sc.nextDouble();
-                System.out.println("enter width of reactangle");
-                double width = sc.nextDouble();
-                System.out.println("perimetre of rectangle " + ((length + width)*2));
-                System.out.println("area of rectangle is " + (length * width));
-                System.out.println(" ");
-                break;
-            case "triangle":
-                System.out.println("enter the value of a");
-                double a = sc.nextDouble();
-                System.out.println("enter the value of b");
-                double b = sc.nextDouble();
-                System.out.println("value of slope is " + Math.sqrt(((a*a) + (b*b))));
-                System.out.println("");
-                break;
-            default:
-                System.out.println("Enter vaild parameters");
-                System.out.println(" ");
-                break;
-        }
+        //     case "square":
+        //         System.out.println("enter area of square");
+        //         double area = sc.nextDouble();
+        //         System.out.println("perimetr od square " + (4*area));
+        //         System.out.println("area of square " + (area * area));
+        //         System.out.println(" ");
+        //         break;
+        //     case "rectangle":
+        //         System.out.println("enter length of rectangle");
+        //         double length = sc.nextDouble();
+        //         System.out.println("enter width of reactangle");
+        //         double width = sc.nextDouble();
+        //         System.out.println("perimetre of rectangle " + ((length + width)*2));
+        //         System.out.println("area of rectangle is " + (length * width));
+        //         System.out.println(" ");
+        //         break;
+        //     case "triangle":
+        //         System.out.println("enter the value of a");
+        //         double a = sc.nextDouble();
+        //         System.out.println("enter the value of b");
+        //         double b = sc.nextDouble();
+        //         System.out.println("value of slope is " + Math.sqrt(((a*a) + (b*b))));
+        //         System.out.println("");
+        //         break;
+        //     default:
+        //         System.out.println("Enter vaild parameters");
+        //         System.out.println(" ");
+        //         break;
+        // }
 
+        // for(int i = 1;i< 20;i++){
+        //     if(i<10){
+        //         if((i%2) != 0){
+        //             System.out.println(i);
+        //         }
+        //     }
+
+        //     if(i>10){
+        //         if((i%2) == 0){
+        //             System.out.println(i-10);
+        //         }
+        //     }
+            
+        // }
+
+        //prime number
+
+    //     int val = 9;
+    //     if(val <=1){
+    //         System.out.println(false);
+    //     }
+    //     else{
+    //         boolean prime = true;
+    //         for (int i = 2; i <= Math.sqrt(val); i++) {
+    //             if(val%i == 0){
+    //                 prime = false;
+    //                 break;
+    //         }
+    //     }
+    //     System.out.println(prime);
+    // }
+
+    
     }
 }
