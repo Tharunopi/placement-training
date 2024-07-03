954e687be097be218a5e2a6ddcc618147a96e969
Tharun Atithya <tharunaadhi6@gmail.com>
2024-07-03 16:54:48 +0530

    last

--- day3.java
+++ day3.java
@@ -69,28 +69,65 @@ public class day3 {
         // }
         // System.out.println(v);
 
-        int a[] = {1,2,3,4};
-        int b[] = {4,3,2,1};
-        boolean v = true;
-        for (int i= 0;i<a.length;i++) {
-            boolean f = false;
-            for (int j = 0; j<a.length; j++) {
-                System.out.println(i + " " + j);
-                if(a[i] == b[j]){
-                    f = true;
-                    break;
+        // int a[] = {1,2,3,4};
+        // int b[] = {4,3,2,1};
+        // boolean v = true;
+        // for (int i= 0;i<a.length;i++) {
+        //     boolean f = false;
+        //     for (int j = 0; j<a.length; j++) {
+        //         System.out.println(i + " " + j);
+        //         if(a[i] == b[j]){
+        //             f = true;
+        //             break;
+        //         }
+        //     }
+        //          if(f==false){
+        //             v = false;
+        //             break;
+        //         }
+        
+    // }
+    // System.out.println(v);
+    // }
+
+        // int a[] = {-1,-2,-3,-4};
+        // int max = 0;
+        // max = a[1];
+        // for(int i:a){
+        //     if(max<i){
+        //         max = i;
+        //     }
+        // }
+        // System.out.println(max);
+
+        // int a[] = {-2,-3,-1,-4};
+        // int max = -11111;
+        // int sec = -1111;
+        // for(int i:a){
+        //     if(max<i){
+        //         sec = max;
+        //         max = i;
+        //     }else if(sec<i && i != max){
+        //         sec = i;
+        //     }
+        // }
+        // System.out.println(max);
+        // System.out.println(sec);  
+
+        int a[] = {2,1,2,3,4,1,5,6,3,3};
+        int count = 0;
+        for (int i = 0;i<a.length;i++) {
+            for (int j = 0; j < a.length; j++) {
+                if(j != i){
+                    if(a[j] == a[i]){
+                        System.out.println(a[i]);
+                        break;
+                    }
                 }
             }
-                 if(f==false){
-                    v = false;
-                    break;
-                }
             
-            
-        
         }
-        System.out.println(v);
-        }
+    }
     
     }
 
