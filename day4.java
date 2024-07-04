0beb03cda57ca2979605b321a07032ce506f8ba0
Tharun Atithya <tharunaadhi6@gmail.com>
2024-07-04 10:09:09 +0530

    early

---
+++ day4.java
@@ -0,0 +1,47 @@
+public class day4 {
+    public static void main(String[] args) {
+//         int a[] = {5,2,3};
+//         boolean b = true;
+//         String ones[] = {"one hunderd and", "two hunderd and", "three hunderd and", "four hunderd and", "five hunderd and", "six hunderd and", "seven hunderd and", "eight hunderd and", "nine hunderd and"};
+//         String twos[] = {"ten", "twenty", "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninty"};
+//         String three[] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+//         for (int i: a) {
+//             for (int j = 0; j < ones.length; j++) {
+//                 if(i==j){
+//                     System.out.print(ones[j-1] + " ");
+//                     b = false;
+//                 }
+//             }
+//         if (b == false) {
+//             break;
+            
+//         }
+//     }
+//         for (int x = 0; x<twos.length;x++) {
+//             if(x==1){
+//             for (int j = 0; j < twos.length; j++) {
+//                 if(x==j){
+//                     System.out.print(twos[j] + " ");
+//                     b = false;
+//                 }
+//             }
+//         if (b == false) {
+//             break;
+            
+//         }
+//     }
+//     }
+//     for (int x = 0; x<three.length;x++) {
+//         if(x==2){
+//         for (int j = 0; j < three.length; j++) {
+//             if(x==j){
+//                 System.out.print(three[j] + " ");
+//                 b = false;
+//             }
+//         }
+//     if (b == false) {
+//         break;
+        
+//     }
+// }
+// }
