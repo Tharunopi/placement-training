e633c8c662f68b6135cf7309accbc68f19a50e33
Tharun Atithya <tharunaadhi6@gmail.com>
2024-07-12 10:00:09 +0530

    last

---
+++ day10.java
@@ -0,0 +1,20 @@
+public class day10 {
+    public static void main(String[] args) {
+        // exceptional handling
+        //try
+        //catch
+        //throw
+        //throws 
+        //final
+
+        // int a = 2;
+        // int b = 0;
+        // if(b==0){
+        //     System.out.println("error");
+        // }else{
+        //     System.out.println(a/b);
+        // }
+
+        
+    }
+}
